2f43354f7aefe847a4158d90edfc5c5805c1906e
lijiannan <2484503@qq.com>
2019-12-09 09:35:16 +0800

    modifu import

--- src/main/java/com/example/demo/controller/CountryController.java
+++ src/main/java/com/example/demo/controller/CountryController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.param.CountryParam;
 import com.example.demo.model.entity.Country;
+import com.example.demo.model.param.CountryParam;
 import com.example.demo.services.CountryService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
